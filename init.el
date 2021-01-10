53fa459e6c34809db4c5ee68c4c6d7ac6aba5e29
wild0rez <maurodirollo@gmail.com>
2021-01-10 19:28:51 +0100

    Modifiche Varie Aggiunti i numeri relativi e ivy

--- init.el
+++ init.el
@@ -3,6 +3,12 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; remove useless GUI items
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t)
+
 ;; use-package
 (unless (package-installed-p 'use-package) 
   (package-refresh-contents) 
