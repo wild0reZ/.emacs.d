4f8034001ae0ab9986b063e501107ffb9916f3e5
wild0rez <maurodirollo@gmail.com>
2021-01-10 20:26:55 +0100

    Projectile

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(counsel swiper ivy ace-window elisp-format magit evil-matchit evil-commentary evil-surround evil use-package)))
+   '(counsel-projectile projectile smartparens smartparens-config counsel swiper ivy ace-window elisp-format magit evil-matchit evil-commentary evil-surround evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -97,3 +97,26 @@
   (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
   (global-set-key (kbd "C-c v") 'ivy-push-view)
   (global-set-key (kbd "C-c V") 'ivy-pop-view))
+
+;; smartparens
+(use-package smartparens
+  :ensure t
+  :config
+  (smartparens-global-mode t))
+
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Documenti/Projects")
+    (setq projectile-project-search-path '("~/Documenti/Projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :ensure t
+  :config (counsel-projectile-mode))
